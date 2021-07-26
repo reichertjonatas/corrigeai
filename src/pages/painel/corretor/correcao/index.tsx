5029f7ebef2b5f9f42d1113c970c11ef45bcfa3d
kellvembarbosa <kellvembarbosa@icloud.com>
2021-07-26 14:10:35 +0100

    versão melhorada do editor do corretor

--- src/pages/painel/corretor/correcao/index.tsx
+++ src/pages/painel/corretor/correcao/index.tsx
@@ -41,12 +41,15 @@ function Correcao() {
     const [annotations, setAnnotations] = useState([])
     const [annotation, setAnnotation] = useState({})
 
+    
+
+
 
     function renderHighlight({ annotation, active }: any) {
         const { geometry } = annotation
         if (!geometry) return null
 
-        var boxStyle : any = {
+        var boxStyle: any = {
             border: 'solid 1px black',
             boxShadow: active
                 && '0 0 20px 20px rgba(255, 255, 255, 0.3) inset'
@@ -91,7 +94,7 @@ function Correcao() {
                     borderRadius: '1rem',
                     border: `solid 1px ${cor}`,
                     backgroundColor: `${cor}90`,
-                    padding: '1rem',
+                    // padding: '1rem',
                     display: 'flex',
                     justifyContent: 'center',
                     alignItems: 'center',
@@ -99,7 +102,7 @@ function Correcao() {
                     boxShadow: active && `0 0 12px 12px ${cor}90 inset`,
                 }
                 break;
-        
+
             default:
                 boxStyle = {
                     borderRadius: '1rem',
@@ -189,7 +192,113 @@ function Correcao() {
             >
                 <div style={{ marginBottom: '0.25rem', fontSize: '0.8rem', fontWeight: 'bold' }}>{nomeCompetencia}</div>
                 {annotation.data && annotation.data.text}
+                <div style={{textAlign: 'center', marginTop: '0.4rem'}}>
+                    <button style={{ 
+                        padding: 2,
+                        minWidth: '50%',
+                        borderRadius: '0.5rem', 
+                        backgroundColor: `white`, 
+                        border: '1px solid white', 
+                        color: cor,
+                        //textTransform: 'uppercase',
+                        fontWeight: 600,
+                        fontSize: '0.95rem',
+                        'fontFamily': "Poppins, sans-serif"
+
+                    }}>Exluir</button>
+                </div>
             </div>
+        )
+    }
+
+
+
+    function renderSelector({ annotation, active }: any) {
+        const { geometry } = annotation
+        if (!geometry) return null
+
+        var boxStyle: any = {
+            border: 'solid 1px black',
+            boxShadow: active
+                && '0 0 20px 20px rgba(255, 255, 255, 0.3) inset'
+        }
+
+        var cor = 'red';
+
+        console.log(annotation)
+
+        switch (annotation.data.competencia ?? 1) {
+            case 2:
+                cor = '#fb5400';
+                break;
+            case 3:
+                cor = '#b5179e';
+                break;
+            case 4:
+                cor = '#fcbe21';
+                break;
+            case 5:
+                cor = '#8ac925';
+                break;
+            default:
+                cor = '#3f37c9';
+                break;
+        }
+
+        switch (annotation.data.editorType) {
+            case 2:
+                boxStyle = {
+                    // borderRadius: '1rem',
+                    borderBottom: `solid 2px ${cor}`,
+                    borderTop: 'none',
+                    borderLeft: 'none',
+                    borderRight: 'none',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: cor,
+                    backgroundColor: `${cor}20`,
+                    boxShadow: active && `0 0 12px 12px ${cor}90 inset`,
+                }
+                break;
+            case 3:
+                boxStyle = {
+                    borderRadius: '1rem',
+                    border: `solid 1px ${cor}`,
+                    backgroundColor: `${cor}90`,
+                    //padding: '1rem',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: cor,
+                    boxShadow: active && `0 0 12px 12px ${cor}90 inset`,
+                }
+                break;
+
+            default:
+                boxStyle = {
+                    borderRadius: '1rem',
+                    // border: `solid 1px ${cor}`,
+                    backgroundColor: `${cor}90`,
+                    // padding: '1rem',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bold',
+                    fontSize: '1.8rem',
+                    alignItems: 'center',
+                    color: cor,
+                    boxShadow: active && `0 0 12px 12px ${cor}90 inset`,
+                    '-webkit-text-stroke': '1.5px white'
+                }
+                break;
+        }
+
+        return (
+            <Box
+                geometry={geometry}
+                style={boxStyle}
+            >
+                {annotation.data.editorType == 1 && 'X'}
+            </Box>
         )
     }
 
@@ -202,13 +311,18 @@ function Correcao() {
         setAnnotation({})
     }
 
-    const onChange = (annotation: any) => {
-        setAnnotation(annotation)
+    const onChange = (nAnnotation: any) => {
+        setAnnotation({
+            data: {
+                competencia: competencia,
+                editorType: editorType,
+            },
+            ...nAnnotation,
+        })
     }
 
     const onSubmit = (annotation: any) => {
         const { geometry, data } = annotation
-
         setAnnotation({})
         setAnnotations(annotations.concat({
             // @ts-ignore
@@ -221,6 +335,107 @@ function Correcao() {
             }
         }))
     }
+
+    function renderEditor (props : any) {
+        const { geometry } = props.annotation
+        if (!geometry) return null
+
+        var cor = 'black';
+        var nomeCompetencia = '';
+
+        switch (props.annotation.data.competencia) {
+            case 2:
+                cor = '#fb5400';
+                break;
+            case 3:
+                cor = '#b5179e';
+                break;
+            case 4:
+                cor = '#fcbe21';
+                break;
+            case 5:
+                cor = '#8ac925';
+                break;
+            default:
+                cor = '#3f37c9';
+                break;
+        }
+
+        switch (props.annotation.data.competencia) {
+            case 2:
+                nomeCompetencia = 'Competência II';
+                break;
+            case 3:
+                nomeCompetencia = 'Competência III';
+                break;
+            case 4:
+                nomeCompetencia = 'Competência IV';
+                break;
+            case 5:
+                nomeCompetencia = 'Competência V';
+                break;
+            default:
+                nomeCompetencia = 'Competência I';
+                break;
+        }
+
+        const inputStyle = {
+            "&:focus" : { outline: "none" },
+            borderRadius: '0.25rem',
+            border: 'none',
+            padding: '0.2rem 0.4rem',
+            minWidth: '100%',
+            fontSize: '0.8rem'
+        };
+      
+        return (
+          <div
+            style={{
+              background: 'white',
+              padding: 10,
+              marginTop: 4,
+              boxShadow: '7px 7px 5px 0px rgba(50, 50, 50, 0.75); inset',
+              backgroundColor: cor,
+              borderRadius: 6,
+              position: 'absolute',
+              left: `${geometry.x}%`,
+              top: `${geometry.y + geometry.height}%`,
+            }}
+          >
+            <div style={{ fontSize: '0.85rem', fontWeight: 'bold', textAlign: 'start', color: 'white' }}>{nomeCompetencia}</div>
+            <div style={{ fontSize: '0.75rem', color: 'white'}}>Descreva o motivo da nota:</div>
+            <div style={{marginTop: 10, marginBottom: 10}}>
+            <textarea
+                rows={4}
+                cols={30}
+                style={inputStyle}
+              onChange={e => props.onChange({
+                ...props.annotation,
+                data: {
+                  ...props.annotation.data,
+                  text: e.target.value
+                }
+              })}
+            />
+            </div>
+            <div style={{textAlign: 'center'}}>
+                <button style={{ 
+                    padding: 2,
+                    minWidth: '50%',
+                    borderRadius: '0.5rem', 
+                    backgroundColor: `white`, 
+                    border: '1px solid white', 
+                    color: cor,
+                    //textTransform: 'uppercase',
+                    fontWeight: 600,
+                    fontSize: '0.95rem',
+                    'fontFamily': "Poppins, sans-serif"
+
+                }} onClick={props.onSubmit}>Salvar</button>
+            </div>
+          </div>
+        )
+      }
 
     useEffect(() => {
         switch (editorType) {
@@ -235,6 +450,22 @@ function Correcao() {
                 break;
         }
     }, [editorType])
+
+    const getColorActivite = () => {
+        switch (competencia) {
+            case 2:
+                return '#fb5400';
+                break;
+            case 3:
+                return '#b5179e';
+            case 4:
+                return '#fcbe21';
+            case 5:
+                return '#8ac925';
+            default:
+                return '#3f37c9';
+        }
+    }
 
     return (
         <MainLayout menuType={2}>
@@ -254,20 +485,20 @@ function Correcao() {
 
                         <div className={styles.tasks}>
 
-                            <span onClick={() => handlerEditorType(1)} className={editorType == 1 ? `${styles.task} ${styles.active}` : styles.task}>
+                            <span onClick={() => handlerEditorType(1)} className={editorType == 1 ? `${styles.task}` : styles.task} style={{ border: editorType == 1 ? `2px solid ${getColorActivite()}` : 'none' }}>
                                 <span className={styles.img}>
                                     <Image src={CloseIcon} className={styles["img-responsive"]} alt="" />
                                 </span>
                                 <span className={styles.text}>Adicionar &quot;x&quot;</span>
                             </span>
 
-                            <span onClick={() => handlerEditorType(2)} className={editorType == 2 ? `${styles.task} ${styles.active}` : styles.task}>
+                            <span onClick={() => handlerEditorType(2)} className={editorType == 2 ? `${styles.task}` : styles.task} style={{ border: editorType == 2 ? `2px solid ${getColorActivite()}` : 'none' }} >
                                 <span className={styles.img}>
                                     <Image src={PencilIcon} className={styles["img-responsive"]} alt="" />
                                 </span>
                                 <span className={styles.text}>Traçado de lápis</span>
                             </span>
-                            <span onClick={() => handlerEditorType(3)} className={editorType == 3 ? `${styles.task} ${styles.active}` : styles.task}>
+                            <span onClick={() => handlerEditorType(3)} className={editorType == 3 ? `${styles.task}` : styles.task} style={{ border: editorType == 3 ? `2px solid ${getColorActivite()}` : 'none' }}>
                                 <span className={styles.img}>
                                     <Image src={PencilIcon} className={styles["img-responsive"]} alt="" />
                                 </span>
@@ -283,6 +514,8 @@ function Correcao() {
                                 annotations={annotations}
                                 renderContent={renderPopUp}
                                 renderHighlight={renderHighlight}
+                                renderSelector={renderSelector}
+                                renderEditor={renderEditor}
 
                                 type={type}
                                 value={annotation}
