7666d6f4839f37dc760d014e91cf77c495c1cfc6
kellvembarbosa <kellvembarbosa@icloud.com>
2021-09-19 21:30:35 +0100

    git 502

---
+++ src/pages/502.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BadGeteway() {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100vw'}}>
+            <h1>Aguarde um momento!</h1>
+        </div>
+    )
+}
+
+export default BadGeteway
