ddbb38d0060b9bbef345fbb264e1b7101228853e
devcorrigeai <devcorrigeai@gmail.com>
2021-10-04 16:28:45 +0100

    Update MainLayout.tsx
    new tag manager

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -66,8 +66,20 @@ const MainLayout = ({ children, menuType = 1, role = "authenticated" }: MainLayo
 
     return (
         <>
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7GCH9D"
-height="0" width="0" style={{ display:'none', visibility:'hidden'}}></iframe></noscript>
+            <script dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-T7GCH9D');`,
+                }}>
+            </script>
+
+
+
+            <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7GCH9D"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+
 
             <div className="bg-green"></div>
             <div className="container-full">
